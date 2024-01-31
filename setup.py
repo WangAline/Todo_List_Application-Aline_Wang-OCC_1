a139893ed458c3bacd51f76ccee00190ded9c6e5
WangAline <146206671+WangAline@users.noreply.github.com>
2024-01-31 07:16:10 +0100

    Add setup.py file for Todo-List-Application

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+ name='{Todo-List-Application}',
+ version='1.0',
+ author='{WangAline}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
